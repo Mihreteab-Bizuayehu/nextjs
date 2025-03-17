a56bae5a25ae47bf5b6398f41db4b3801fbae039
mihreteab-bizuayehu <lijmihret@gmail.com>
2025-03-17 12:14:52 +0300

    Layout is setup successfully

---
+++ ecommerce/app/(front)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-medium mt-4 p-4">Latest Projects</h1>
+      
+    </>
+  );
+}
